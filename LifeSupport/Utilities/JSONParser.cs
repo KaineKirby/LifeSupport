fef896746350b1509c8c3584aee3614b991e4006
Chavezzz <47615464+Chavezzz@users.noreply.github.com>
2019-02-25 17:14:51 -0600

    Update JSONParser.cs
    This method reads a json file and puts the data into an object.

--- LifeSupport/Utilities/JSONParser.cs
+++ LifeSupport/Utilities/JSONParser.cs
@@ -1,12 +1,29 @@
-﻿using System;
+﻿using LifeSupport.Config;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace LifeSupport.Utilities
 {
-    class JSONParser
+     class JSONParser
     {
+        public JSONParser()
+        {
+
+        }
+        public static T readJSON<T>(T obj, String jsonFile)
+        {
+             using (StreamReader r = new StreamReader(jsonFile))
+            {
+                string JSONdata = r.ReadToEnd();
+                obj = JsonConvert.DeserializeObject<T>(JSONdata);
+            }
+            return obj;
+             
+        }
     }
 }
