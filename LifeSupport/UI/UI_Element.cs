0c6a9b1c02f1eec1d39f6cf8ac7f31c1743ed32d
Dustin-PC\Dustin <1998davery2013@gmail.com>
2019-02-27 06:12:03 -0600

    UI_Element added, will need a lot of work, pushing so I can work on other computer

---
+++ LifeSupport/UI/UI_Element.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+abstract class UI_Element{
+
+    //Screen Position
+    public float XPos;
+    public float YPos;
+    public float scale;
+
+	public UI_Element(float XPos, float YPos, Game game) {
+        this.XPos = XPos;
+        this.YPos = YPos;
+        this.scale = scale;
+	}
+
+
+}
